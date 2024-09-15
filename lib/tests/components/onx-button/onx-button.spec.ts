6944221317120242f55e5c694a2cd17d6e147831
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-09-15 13:22:27 -0500

    chore: remove files

--- lib/tests/components/onx-button/onx-button.spec.ts
+++ lib/tests/components/onx-button/onx-button.spec.ts
@@ -1,26 +1,50 @@
-import { expect, test } from '@sand4rt/experimental-ct-web';
+import { expect, test as base, MountResult } from '@sand4rt/experimental-ct-web';
 import {
   GivenButtonTypeIsReset_WhenClicked_ItShouldResetForm,
   GivenButtonTypeIsSubmit_WhenClicked_ItShouldSubmitForm,
 } from './onx-button.cases';
 import OnxButton from '../../../src/components/onx-button/component';
+import OnxComponent from '../../../src/components/onx-component';
+
+type Fixtures = {
+  customMount: (setup: () => OnxComponent) => Promise<{
+    wrapper: MountResult<HTMLElement>;
+  }>;
+};
+
+const test = base.extend<Fixtures>({
+  customMount: async ({ mount }, use) =>
+    await use(async (setup: () => OnxComponent) => {
+      const component = setup();
+
+      class Wrapper extends HTMLElement {
+        constructor() {
+          super();
+        }
+
+        connectedCallback() {
+          this.appendChild(component);
+        }
+      }
+
+      customElements.define('wrapper', Wrapper);
+
+      const wrapper = await mount(Wrapper);
+
+      return { wrapper };
+    }),
+});
 
 test.describe('OnxButton', () => {
   test('given button variant is primary-filled, when rendered, it should display properly', async ({
+    customMount,
     mount,
   }) => {
-    const component = await mount(OnxButton, {
-      props: {
-        variant: 'primary-filled',
-      },
-      slots: {
-        default: 'Primary Filled',
-      },
-    });
+    const result = await customMount(() => document.createElement('onx-button'));
 
-    await expect(component).toHaveText('Primary Filled');
-    await expect(component).toHaveAttribute('variant', 'primary-filled');
-    await expect(component).toHaveScreenshot();
+    await expect(result.wrapper).toHaveText('Primary Filled');
+    await expect(result.wrapper).toHaveAttribute('variant', 'primary-filled');
+    await expect(result.wrapper).toHaveScreenshot();
   });
 
   test('given button type is submit, when clicked, it should submit form', async ({ mount }) => {
